dce842ae10610293ae7cecf9865cad7207279017
HeechanYang <yhc944@gmail.com>
2020-09-06 23:03:59 +0900

    init

---
+++ src/main/kotlin/info/log/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package info.log.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
---
+++ src/test/kotlin/info/log/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package info.log.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
